fac6b191ad4930d73b0ba33f2a32a173d87b3506
Richard Jones <richard@cottagelabs.com>
2020-09-15 16:33:26 +0100

    compile up some static pages for tas, tjs, and api docs

--- compile_markdown.py
+++ compile_markdown.py
@@ -3,14 +3,14 @@ import markdown, os
 files = ["apidocs", "transformative-agreements", "transformative-journals"]
 
 for f in files:
-    with open(os.path.join("markdown", f + ".md") as md:
+    with open(os.path.join("markdown", f + ".md")) as md:
         html = markdown.markdown(md.read(), extensions=["markdown.extensions.fenced_code"])
-    with open(os.path.join("pagefragments", f + ".html", "w") as out:
+    with open(os.path.join("pagefragments", f + ".html"), "w") as out:
         out.write(html)
-    with open(os.path.join("pagefragments", f + ".html", "r") as frag,
-            open(os.path.join("pagefragments", "header.html") as header,
-            open(os.path.join("pagefragments", "footer.html") as footer:
+    with open(os.path.join("pagefragments", f + ".html")) as frag, \
+            open(os.path.join("pagefragments", "header.html")) as header, \
+            open(os.path.join("pagefragments", "footer.html")) as footer:
         full = header.read() + frag.read() + footer.read()
-    with open(os.path.join("content", f + "html", "w") as final:
+    with open(os.path.join("content", f + ".html"), "w") as final:
         final.write(full)
 
