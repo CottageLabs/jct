3827d1e55a54636621401c1500ec3ec8fd1615f7
Anusha Ranganathan <anusha3@gmail.com>
2020-11-03 13:52:48 +0000

    Code to display unknown results returned by the api

--- static/detailed_results.js
+++ static/detailed_results.js
@@ -3,48 +3,79 @@ jct.explain = (q) => {
     detailed_results.innerHTML = "";
     let compliant_routes = `<h2>Compliant Routes</h2>`
     let noncompliant_routes = `<h2>Non-Compliant Routes</h2>`
+    let unknown_routes = `<h2>Unknown Routes</h2>`
     let compliant_routes_number = 0;
     let noncomplicant_routes_number = 0;
+    let unknown_routes_number = 0;
 
     q.results.forEach((r) => {
-        if (r.compliant !== 'unknown') {
-            let route = `
-            <h3>` + jct.COMPLIANCE_ROUTES_LONG[r.route] + `</h3>
-            <p>You <b>`  + (r.compliant === "yes" ? "are Plan S compliant" : "cannot comply with Plan S") + `</b> on this route (` + jct.COMPLIANCE_ROUTES_LONG[r.route] + `).</p>
-            <p>The following checks were carried out to determine that this is`  + (r.compliant === "yes" ? "" : "not") + ` a compliant route:</p>
-        `
-            r.log.forEach((log) => {
-                route += "<ul><li>" + log.action + "</li>"
-                route += "<ul><li>" + log.result + "</li>"
-                if (log.url) {
-                    route += "<li>" + log.url + "</li>"
-                }
-                route += "</ul></ul>"
-            })
+        if (r.compliant === "yes") {
+            statement = "You <b>are Plan S compliant</b> on this route";
+            explanation = "The following checks were carried out to determine that this is a compliant route:"
+        } else if (r.compliant === "no") {
+            statement = "You <b>cannot comply with Plan S</b> on this route";
+            explanation = "The following checks were carried out to determine that this is not a compliant route:"
+        } else {
+            statement = "We are <b>unable to determine if you are complaint</b> on this route";
+            explanation = "The following checks were carried out to determine compliance:"
+        }
 
-            if (r.compliant === "yes") {
-                compliant_routes_number++;
-                compliant_routes += route;
-            } else {
-                noncomplicant_routes_number++;
-                noncompliant_routes += route;
+        let route = `
+        <h3>` + jct.COMPLIANCE_ROUTES_LONG[r.route] + `</h3>
+        <p>`  + statement + ` (` + jct.COMPLIANCE_ROUTES_LONG[r.route] + `).</p>
+        <p>`  + explanation + `</p>`
+
+        r.log.forEach((log) => {
+            route += "<ul><li>" + log.action + "</li>"
+            route += "<ul><li>" + log.result + "</li>"
+            if (log.url) {
+                route += "<li>" + log.url + "</li>"
             }
+            route += "</ul></ul>"
+        })
+
+        if (r.compliant === "yes") {
+            compliant_routes_number++;
+            compliant_routes += route;
+        } else if (r.compliant === "no") {
+            noncomplicant_routes_number++;
+            noncompliant_routes += route;
+        } else {
+            unknown_routes_number++;
+            unknown_routes += route;
         }
     });
+
+    let blurb_for_count = "";
+    let blurb_routes = [ "compliant", "non-compliant", "undetermined" ];
+    let blurb_line_construct = [ ", ", " and ", "." ];
+    [compliant_routes_number, noncomplicant_routes_number, unknown_routes_number].forEach((num, index) => {
+        switch(num) {
+            case 0:
+                blurb_for_count += `no ` + blurb_routes[index] + ` routes` + blurb_line_construct[index];
+                break;
+            case 1:
+                blurb_for_count += `1 ` + blurb_routes[index] + ` route` + blurb_line_construct[index];
+                break;
+            default:
+                blurb_for_count += num + ` ` + blurb_routes[index] + ` routes` + blurb_line_construct[index];
+                break;
+        }
+    })
 
     let text =
         `
         <h3>Your query</h3>
-        
+
         <p>You asked us to calculate whether you are Plan S compliant under the following conditions:
-        
+
         <ul>
             <li>Journal: </li>
             <ul class="second">
                 <li> ISSN: ` + q.request.journal[0].id + `</li>
                 <li> Title: ` + q.request.journal[0].title + `</li>
                 <li> Publisher: ` + (q.request.journal[0].publisher !== undefined ? q.request.journal[0].publisher : "Not known") + `</li>
-            </ul> 
+            </ul>
             <li>Funder: ` + q.request.funder[0].title + `</li>`
 
     if ((q.request.institution.length > 0)){
@@ -62,15 +93,16 @@ jct.explain = (q) => {
 
     text +=
         `</ul>
-        
-        We carried out this query at ` + new Date(q.request.started).toUTCString() +`, and found ` +
-        compliant_routes_number + ` compliant routes and ` + noncomplicant_routes_number + ` non-compliant routes.
+
+        We carried out this query at ` + new Date(q.request.started).toUTCString() +
+        `, and found ` + blurb_for_count + `
         </p>
     `
 
-
-
-    let elem = htmlToElement("<div id='detailed_result_text'>" + text + (compliant_routes_number > 0 ? compliant_routes : "") + (noncomplicant_routes_number > 0 ? noncompliant_routes : "") + "</div>");
+    let elem = htmlToElement("<div id='detailed_result_text'>" + text +
+        (compliant_routes_number > 0 ? compliant_routes : "") +
+        (noncomplicant_routes_number > 0 ? noncompliant_routes : "") +
+        (unknown_routes_number > 0 ? unknown_routes : "") + "</div>");
     detailed_results.append(elem);
 
     jct.d.gebi("print").addEventListener("click", () => {
