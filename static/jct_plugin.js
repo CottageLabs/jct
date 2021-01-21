b8b79543c8f181b4d3063dcdfb29d74e723a8f9e
Anusha Ranganathan <anusha3@gmail.com>
2021-01-21 08:35:08 +0000

    Fixed api endpoint to use live

--- static/jct_plugin.js
+++ static/jct_plugin.js
@@ -1,7 +1,7 @@
 
 // -------- api_endpoint --------
 
-window.JCT_API_endpoint = 'https://api.jct.cottagelabs.com';
+window.JCT_API_endpoint = 'https://api.journalcheckertool.org';
 window.JCT_UI_BASE_URL = "https://journalcheckertool.org";
 
 
