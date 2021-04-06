74794c650c0add2b917e8c9fa668af86f9fdf292
Richard Jones <richard@cottagelabs.com>
2021-04-06 14:48:50 +0100

    quick spell, grammar and capitalisation review of the text

--- static/js/api_codes.js
+++ static/js/api_codes.js
@@ -18,7 +18,7 @@ jct.api_codes = {
         },
         'rights_retention_author_advice': {
             description:            'Your funder supports you to use the <a href="https://www.coalition-s.org/faq-theme/rights-licences/" target="_blank"  rel="noopener">'+'' +
-                                    'cOAlition S rights retention strategy</a> as a route to compliance irrespective of the journals self-archiving policy.',
+                                    'cOAlition S rights retention strategy</a> as a route to compliance irrespective of the journal\'s self-archiving policy.',
         },
         'corresponding_authors': {
             description:            'The corresponding author of the submitted article must be based at '+
@@ -53,7 +53,7 @@ jct.api_codes = {
         'FullOA.InProgressDOAJ':    'This journal is currently under review for potential inclusion in DOAJ.',
         'FullOA.NotInProgressDOAJ': 'This journal is not currently under review at DOAJ.',
         'FullOA.InDOAJ':            'This journal is present in DOAJ.',
-        'FullOA.Compliant':         'This journal enables you to publish under the following licences that are supported by your funders policy:',
+        'FullOA.Compliant':         'This journal enables you to publish under the following licences that are supported by your funder\'s policy:',
             'FullOA.Compliant.Properties': {
             'licence':              ''
         },
@@ -78,13 +78,13 @@ jct.api_codes = {
                                     "Plan S via self-archiving. Data from Open Access Button Permissions (OAB Permissions) is used " +
                                     "to see if the publisher's policy of self-archiving enables compliance. If it does not or if an " +
                                     "unknown answer has been returned then the cOAlition S Implementation Roadmap data is checked " +
-                                    "to see if cOAlition S’s Rights Retention Strategy provides a route to compliance:",
+                                    "to see if cOAlition S's Rights Retention Strategy provides a route to compliance:",
             'no':                   'The following checks were carried out to determine that this is not a compliant route:',
             'unknown':              'The following checks were carried out to determine compliance:',
         },
         'SA.InOAB':                 'This journal is present in OAB Permissions.',
         'SA.NotInOAB':              'This journal is not present in OAB Permissions.',
-        'SA.OABNonCompliant':       "This journals self-archiving policy does not enable compliance with your funder's open access policy, for the following reason(s):",
+        'SA.OABNonCompliant':       "This journal's self-archiving policy does not enable compliance with your funder's open access policy, for the following reason(s):",
         'SA.OABNonCompliant.Properties': {
             'version':              'The manuscript version that can be archived is:',
             'embargo':              'There is an embargo period (in months):',
@@ -104,50 +104,50 @@ jct.api_codes = {
                                     '<a href="https://www.coalition-s.org/faq-theme/rights-licences/" target="_blank"  rel="noopener">Plan S Rights Retention Strategy</a>.',
         'SA.FunderRRActive':        'Your funder has implemented the ' +
                                     '<a href="https://www.coalition-s.org/faq-theme/rights-licences/" target="_blank"  rel="noopener">Plan S Rights Retention Strategy</a>. ' +
-                                    'Rights retention takes precedence over the journals self-archiving policy. It provides a route to ' +
+                                    'Rights retention takes precedence over the journal\'s self-archiving policy. It provides a route to ' +
                                     'compliance irrespective of publisher imposed restrictions or embargo periods.',
-        'SA.Unknown':               'We are unable to determine if this journals provides a route to compliance via self-archiving due to missing information.',
+        'SA.Unknown':               'We are unable to determine if this journal provides a route to compliance via self-archiving due to missing information.',
         'SA.NonCompliant':          'Self-archiving is not a route to compliance when publishing in this journal.',
         'SA.Compliant':             'Self-archiving can be a route to compliance when publishing in this journal.'
     },
     ta: {
         'name':                     'Transformative Agreement Route',
         'statement': {
-            'yes':                  'You are able to comply with Plan S via a Transformative agreement.',
-            'no':                   'You are not able to <b>comply with Plan S</b> via a Transformative agreement.',
-            'unknown':              'We are <b>unable to determine</b> if you are able to comply with Plan S via a Transformative agreement.',
+            'yes':                  'You are able to comply with Plan S via a Transformative Agreement.',
+            'no':                   'You are not able to <b>comply with Plan S</b> via a Transformative Agreement.',
+            'unknown':              'We are <b>unable to determine</b> if you are able to comply with Plan S via a Transformative Agreement.',
         },
         'explanation': {
-            'yes':                  'The following checks were carried out on the JCT’s Transformative Agreement Index to ' +
-                                    'determine if a transformative agreements is available that would enable compliance:',
-            'no':                   'The following checks were carried out on the JCT’s Transformative Agreement Index to ' +
-                                    'determine if a transformative agreements is available that would enable compliance:',
-            'unknown':              'The following checks were carried out on the JCT’s Transformative Agreement Index to determine compliance:',
+            'yes':                  'The following checks were carried out on the JCT\'s Transformative Agreement Index to ' +
+                                    'determine if a Transformative Agreement is available that would enable compliance:',
+            'no':                   'The following checks were carried out on the JCT\'s Transformative Agreement Index to ' +
+                                    'determine if a Transformative Agreement is available that would enable compliance:',
+            'unknown':              'The following checks were carried out on the JCT\'s Transformative Agreement Index to determine compliance:',
         },
-        'TA.NoTA':                  'No transformative agreement containing the selected journal and institution was found within our database.',
-        'TA.Exists':                'A transformative agreement containing the selected journal and institution was found within our database.',
-        'TA.NotActive':             'Our database shows that the transformative agreement containing the selected journal and institution has expired.',
-        'TA.Active':                'Our database shows that the transformative agreement containing the selected journal and institution is active.',
-        'TA.Unknown':               'We do not have sufficient information to determine if a transformative agreement is available to provide a route to compliance.',
-        'TA.NonCompliant':          'There is no transformative agreement available to provide a route to compliance.',
-        'TA.Compliant':             'A transformative agreement is available that can provide a route to compliance.',
+        'TA.NoTA':                  'No Transformative Agreement containing the selected journal and institution was found within our database.',
+        'TA.Exists':                'A Transformative Agreement containing the selected journal and institution was found within our database.',
+        'TA.NotActive':             'Our database shows that the Transformative Agreement containing the selected journal and institution has expired.',
+        'TA.Active':                'Our database shows that the Transformative Agreement containing the selected journal and institution is active.',
+        'TA.Unknown':               'We do not have sufficient information to determine if a Transformative Agreement is available to provide a route to compliance.',
+        'TA.NonCompliant':          'There is no Transformative Agreement available to provide a route to compliance.',
+        'TA.Compliant':             'A Transformative Agreement is available that can provide a route to compliance.',
     },
     tj: {
         'name':                     'Transformative Journal Route',
         'statement': {
-            'yes':                  'This journal is a Transformative journal and therefore you <b>can comply with Plan S</b> via this route.',
-            'no':                   'This journal is not a Transformative journal and therefore you <b>cannot comply with Plan S</b> via this route.',
-            'unknown':              'We are unable to determine if this journal is a Transformative journal and therefore <b>unable to determine compliance</b> via this route.',
+            'yes':                  'This journal is a Transformative Journal and therefore you <b>can comply with Plan S</b> via this route.',
+            'no':                   'This journal is not a Transformative Journal and therefore you <b>cannot comply with Plan S</b> via this route.',
+            'unknown':              'We are unable to determine if this journal is a Transformative Journal and therefore <b>unable to determine compliance</b> via this route.',
         },
         'explanation': {
             'yes':                  'The following checks were carried out to determine that this is a compliant route:',
             'no':                   'The following checks were carried out to determine that this is not a compliant route:',
             'unknown':              'The following checks were carried out to determine compliance:',
         },
-        'TJ.NoTJ':                  'This journal is not a transformative journal.',
-        'TJ.Exists':                'This journal is a transformative journal.',
-        'TJ.NonCompliant':          'As this journal is not a transformative journal, this route to compliance is not available.',
-        'TJ.Compliant':             'This transformative journal provides a route to compliance.',
+        'TJ.NoTJ':                  'This journal is not a Transformative Journal.',
+        'TJ.Exists':                'This journal is a Transformative Journal.',
+        'TJ.NonCompliant':          'As this journal is not a Transformative Journal, this route to compliance is not available.',
+        'TJ.Compliant':             'This Transformative Journal provides a route to compliance.',
     }
 }
 
