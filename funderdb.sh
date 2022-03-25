66ff8a02108367b17d6d6b3e83c0bcd2eed07b11
Richard Jones <richard@cottagelabs.com>
2022-03-25 14:29:57 +0000

    update funder autocomplete list for latest ukri changes

--- funderdb.sh
+++ funderdb.sh
@@ -4,11 +4,11 @@
 FUNDERDB=../jct-funderdb
 FDB2HERE=../jct
 
-(cd $FUNDERDB || exit; python config.py)
+#(cd $FUNDERDB || exit; python config.py)
 (cd $FUNDERDB || exit; python funders.py)
-(cd $FUNDERDB || exit; python lang.py)
+#(cd $FUNDERDB || exit; python lang.py)
 (cd $FUNDERDB || exit; git log --pretty=format:'%h' -n 1 > $FDB2HERE/funderdb.git)
 
-cp $FUNDERDB/config/default_config.js static/js
-cp $FUNDERDB/language/default_lang.js static/js
+#cp $FUNDERDB/config/default_config.js static/js
+#cp $FUNDERDB/language/default_lang.js static/js
 cp $FUNDERDB/autocomplete/funders.js static/js
